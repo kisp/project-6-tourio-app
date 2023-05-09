f1f20ccc683f2d2b5a2cbcba5e4aeda8bd7bd3d6
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-05-09 11:18:41 +0200

    GET id works

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,21 @@
-import { places } from '../../../../lib/db.js';
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
+  if (request.method === "GET") {
+    if (!id) {
+      return;
+    }
 
-  if (!id) {
-    return;
+    const place = await Place.findOne({ _id: id });
+
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    response.status(200).json(place);
   }
-
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
-  }
-
-  response.status(200).json(place);
 }
