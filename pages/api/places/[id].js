5d248b39b218ddbf6cf6ab2bc32302d31fe011b0
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-05-09 15:44:21 +0200

    Delete works

--- pages/api/places/[id].js
+++ pages/api/places/[id].js
@@ -4,18 +4,32 @@ import Place from "../../../db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
 
-  const { id } = request.query;
-  if (request.method === "GET") {
-    if (!id) {
-      return;
-    }
+  let { id } = request.query;
+  id = id.toLowerCase();
 
+  if (!/^[a-z0-9]{24}$/.test(id)) {
+    return response.status(404).json({ status: "Not found" });
+  }
+
+  if (request.method === "GET") {
     const place = await Place.findById(id);
 
     if (!place) {
       return response.status(404).json({ status: "Not found" });
     }
 
-    response.status(200).json(place);
+    return response.status(200).json(place);
   }
+
+  if (request.method === "DELETE") {
+    const place = await Place.findByIdAndDelete(id);
+
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    return response.status(200).json(place);
+  }
+
+  return response.status(404).json({ status: "Not found" });
 }
