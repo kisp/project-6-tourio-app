9fd1f0657aceea72a98c7552c419f2276752ed09
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-05-09 12:30:36 +0200

    Respond with 500 on /api/places

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -2,28 +2,32 @@ import dbConnect from "../../../db/connect";
 import Place from "../../../db/models/Place";
 
 export default async function handler(request, response) {
-  await dbConnect();
+  try {
+    await dbConnect();
 
-  if (request.method === "GET") {
-    const places = await Place.find();
-    return response.status(200).json(places);
-  }
-
-  if (request.method === "POST") {
-    try {
-      const { name, location, image, mapURL, description } = request.body;
-      const place = new Place();
-      place.name = name;
-      place.location = location;
-      place.image = image;
-      place.mapURL = mapURL;
-      place.description = description;
-      await place.save();
-      return response.status(201).json(place);
-    } catch (error) {
-      return response.status(422).json(error);
+    if (request.method === "GET") {
+      const places = await Place.find();
+      return response.status(200).json(places);
     }
-  }
 
-  return response.status(404).json({ message: "not found" });
+    if (request.method === "POST") {
+      try {
+        const { name, location, image, mapURL, description } = request.body;
+        const place = new Place();
+        place.name = name;
+        place.location = location;
+        place.image = image;
+        place.mapURL = mapURL;
+        place.description = description;
+        await place.save();
+        return response.status(201).json(place);
+      } catch (error) {
+        return response.status(422).json(error);
+      }
+    }
+
+    return response.status(404).json({ message: "not found" });
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 }
